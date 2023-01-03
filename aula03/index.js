f014d1e93ccef84142b6d05f588302dcebb77156
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-01-03 16:16:16 -0300

    Variaveis com let

---
+++ aula03/index.js
@@ -0,0 +1,20 @@
+let name = "João"; // SEMPRE UTILIZE LET PARA VARIAVEIS!!! - String
+
+console.log(name," nasceu em 2004");
+console.log("A cor favorita de ", name, "é amarelo");
+console.log(name, "tem olhos castanhos e cabelo cacheado");
+
+// se não tivesse nenhum valor, retornaria undefined 
+// Não podemos criar variáveis com palavras reservadas
+// Variáveis precisam ter nomes significativos 
+// Não pode começar o nome de uma variável com um número. Ex: let 1nome
+// Não podem conter espaços ou traços 
+
+// utilizamos camelCase. Ex: nomeCompletoCliente 
+// Case-Sensitive. nomeCliente é diferente de nomecliente
+// Não utilize VAR, utilize LET
+
+let nomeCliente = "Letícia";
+let nomecliente = "Rodrigues"; 
+
+console.log(nomeCliente, nomecliente)
