ae56b67c78cd3de055451e157ee7d1cebcd02d5e
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-01-04 14:43:03 -0300

    Let vs Var

---
+++ aula05/index.js
@@ -0,0 +1,9 @@
+var nome = 'Letícia';
+var nome = 'Rodrigues';
+
+console.log(nome) // vai sair rodrigues, já que foi o último a ser declarado 
+
+let cor = "Azul";
+cor = "Rosa";
+
+console.log(cor); // Vai sair rosa, já que foi redeclarado
