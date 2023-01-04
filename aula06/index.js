34364622c993534dfec35e7483ce0a26c7ea5e9f
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-01-04 14:57:08 -0300

    Tipos de dados primitivos

---
+++ aula06/index.js
@@ -0,0 +1,17 @@
+const nome = "Choi Jongho"; // String
+const age = 23; // Number
+
+let nomeAluno; // undefined = não aponta pra local nenhum na memoria
+let sobrenomeAluno = null; // Nulo = não ter valor na memoria (lógico])
+
+console.log(typeof nomeAluno) // retorna number
+
+const aprovado = true; // boolean - true or false
+
+const a = [1 , 2];
+const b = a;
+
+console.log(a,b)
+
+b.push(3); //adicionou mais um
+console.log(b)
