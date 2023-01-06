a63e866379ccc7c00472ee3f5283ceae91376a98
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-01-06 16:25:21 -0300

    Operadores de Comparacao

---
+++ aula21/index.js
@@ -0,0 +1,20 @@
+/* --------------------------------
+OPERADORES DE COMPARAÇÃO
+> maior que 
+>= maior que ou igual a 
+< menor que 
+<= menor que ou igual a 
+== igualdade // n usamos **
+=== igualdade estrita (valor e tipo)
+!= diferente (valor) // n usamos **
+!== diferente estrito (valor e tipo)
+-----------------------------------*/ 
+
+console.log(10 > 5); // retorna true
+
+const comp = 10<= 9; //n precisa ser maior ou igual, precisa ser só uma
+console.log(comp); 
+
+const num1 = 10;
+const num2 = '10';
+console.log(num1 === num2);
