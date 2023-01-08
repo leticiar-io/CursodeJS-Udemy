2da550e409d377e733e629ffa2aed599d0b97af1
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-01-08 20:19:15 -0300

    Exercicio - IMC

---
+++ aula26/assets/js/main.js
@@ -0,0 +1,70 @@
+const form = document.querySelector("#formulario");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const inputPeso = e.target.querySelector("#peso");
+  const inputAltura = e.target.querySelector("#altura");
+
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
+
+  if (!peso) {
+    //verificando se ele não é falso (se ele é number)
+    setResultado("Peso Inválido!", false);
+    return;
+  }
+  if (!altura) {
+    setResultado("Altura Inválida", false);
+    return;
+  }
+
+  const imc = getImc(peso, altura);
+  const nivelImc = getLevelImc(imc);
+
+  const msg = `Seu IMC é ${imc} e você está no nível ${nivelImc}`;
+  setResultado(msg, true);
+});
+
+function getLevelImc(imc) {
+  const level = [
+    "Abaixo do peso",
+    "Peso Normal",
+    "Sobrepeso",
+    "Obesidade grau 1",
+    "Obesidade grau 2",
+    "Obesidade grau 3",
+  ];
+
+  if (imc >= 39.9) return level[5];
+  if (imc >= 34.9) return level[4];
+  if (imc >= 29.9) return level[3];
+  if (imc >= 24.9) return level[2];
+  if (imc >= 18.5) return level[1];
+  if (imc < 18.5) return level[0];
+}
+
+function getImc(peso, altura) {
+  const imc = peso / altura ** 2;
+  return imc.toFixed(2);
+}
+
+function criaP() {
+  const p = document.createElement("p");
+  return p;
+}
+
+function setResultado(msg, isValid) {
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML = "";
+
+  const p = criaP();
+  if (isValid) {
+    p.classList.add("paragrafo-resultado");
+  } else {
+    p.classList.add("bad");
+  }
+
+  p.innerHTML = msg;
+
+  resultado.appendChild(p);
+}
