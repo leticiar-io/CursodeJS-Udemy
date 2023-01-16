c932ea291fb638414a51f7206561818fa0d6b782
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-01-16 18:29:35 -0300

    For of - Estrutura de Repeticao

---
+++ aula37/index.js
@@ -0,0 +1,9 @@
+const nome = ["Letícia Rodrigues", "Larissa Rodrigues", "Leanderson", "Paula", "Paulo"];
+
+for (let valor of nome ) {
+    console.log(valor); //retorna cada indice (letra) do nome em caso de string
+} // For of 
+
+/*nomes.forEach(function(valor, indice, array) {
+    console.log(valor, indice, array)
+})*/
