9b9f63bbab1fc50463cbecaa065177ecbbe6c93f
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-01-23 16:47:51 -0300

    exercicio de lógica de programação - 02

---
+++ aula42/index.js
@@ -0,0 +1,11 @@
+function ePaisagem(largura, altura) {
+    return largura >= altura; 
+}
+
+console.log(ePaisagem(1580,2080));
+
+const ePaisagem2 = (largura, altura) => {
+    return largura >= altura; 
+}
+
+console.log(ePaisagem2(1020, 1020))
